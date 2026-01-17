4086763599dcf529b9e43bd15591ba97a158a932
tdem2025 <tdemirchyan2010@gmail.com>
2026-01-16 21:32:56 -0800

    Added the file for the code that will actually be used

---
+++ autonomous.py
@@ -0,0 +1,7 @@
+from codrone_edu.drone import *
+
+drone = Drone()
+drone.pair()
+
+
+drone.close()
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 #Python code
 #Python Test Code To Learn The Basics Of Coding CoDrone EDU
-#This just has the drone do basic movements (takeoff, turn left and right,go up and down, and forward and back).
+#This just has the drone do basic movements (takeoff, turn left and right,go up and down, and forward and back, and land).
 from codrone_edu.drone import *
 
 drone = Drone()
 drone.pair() # pair automatically, may not always work
-# if drone.pair doesn't work then try drone.pair(port_name = 'COM3')    # pair with a specific port (Must be set)
+# if drone.pair doesn't work then try drone.pair(port_name = 'COM3')    # pair with a specific port (Must be set??)
 
 
 drone.takeoff()
