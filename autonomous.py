eaabc9462cbce46a481ae260ab5a5b315ea639af
tdem2025 <tdemirchyan2010@gmail.com>
2026-01-29 20:11:33 -0800

    We coded, yes

--- autonomous.py
+++ autonomous.py
@@ -1,24 +1,21 @@
+#This is autonomous.py :)
 from codrone_edu.drone import *
 
 drone = Drone()
 drone.pair()
 
 drone.takeoff()
-
-drone.move_distance(0, 0, 0.9,1)
+drone.set_drone_LED(0,0,255,100)
+#drone.move_distance(0, 0, 0.9,1)
 #drone.hover(5)
 #drone.set_yaw(0)
 #drone.move_forward(distance=1.7, units="m", speed=1)
-drone.flip("back")
-drone.hover(2)
-drone.flip("back")
-drone.hover(2)
-drone.flip("back")
-drone.hover(2)
-drone.flip("back")
-drone.hover(2)
-drone.flip("back")
-drone.hover(2)
+#drone.set_yaw(0)
+color_data = drone.get_back_color("rgb")
+
+print(color_data)
+drone.set_drone_LED(color_data.r, color_data.g, color_data.b)
+drone.hover()
 
 
 
--- fun.py
+++ fun.py
@@ -1,21 +1,24 @@
-#This is fun.py :)
 from codrone_edu.drone import *
 
 drone = Drone()
 drone.pair()
 
 drone.takeoff()
-drone.set_drone_LED(0,0,255,100)
-#drone.move_distance(0, 0, 0.9,1)
+
+drone.move_distance(0, 0, 0.9,1)
 #drone.hover(5)
 #drone.set_yaw(0)
 #drone.move_forward(distance=1.7, units="m", speed=1)
-#drone.set_yaw(0)
-color_data = drone.get_back_color("rgb")
-
-print(color_data)
-drone.set_drone_LED(color_data.r, color_data.g, color_data.b)
-drone.hover()
+drone.flip("back")
+drone.hover(2)
+drone.flip("back")
+drone.hover(2)
+drone.flip("back")
+drone.hover(2)
+drone.flip("back")
+drone.hover(2)
+drone.flip("back")
+drone.hover(2)
 
 
 
