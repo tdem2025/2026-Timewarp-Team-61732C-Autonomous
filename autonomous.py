abff1af5cfaaf02c6dd97768a2236fd390b9d57d
tdem2025 <tdemirchyan2010@gmail.com>
2026-01-29 22:54:01 -0800

    Fixed line 18 (Yet to be tested)

--- autonomous.py
+++ autonomous.py
@@ -5,18 +5,21 @@ drone = Drone()
 drone.pair()
 
 drone.takeoff()
+
 drone.set_drone_LED(0,0,255,100)
 drone.move_distance(0, 0, 0.9,1)
 drone.hover(5)
 drone.set_yaw(0)
 drone.move_forward(distance=1.7, units="m", speed=1)
 drone.set_yaw(0)
+
 color_data = drone.get_back_color("rgb")
-
 print(color_data)
-drone.set_drone_LED(color_data.r, color_data.g, color_data.b)
+drone.set_drone_LED(*color_data)
+#note, after research putting * "unpacks it"
+#so then it won't dump it in like ((r, g, b)) but instead like (r,g,b)
+#must be tested
 drone.hover()
-
 
 
 drone.land()
