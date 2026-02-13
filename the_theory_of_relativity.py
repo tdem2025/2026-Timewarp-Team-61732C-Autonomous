f1af8df4a08df80eadf4bf4548a1c331f5803fe6
tdem2025 <tdemirchyan2010@gmail.com>
2026-02-13 09:12:57 -0800

    drone was acting weird today

--- the_theory_of_relativity.py
+++ the_theory_of_relativity.py
@@ -1,4 +1,4 @@
-#This is the Autonomous Code
+#This is the theoretical perfect code
 #Import CoDrone EDU Libraries
 from codrone_edu.drone import *
 
@@ -11,15 +11,14 @@ drone.takeoff()
 #Begin by taking off, turning off LED, ascending, and going through first key-hole
 drone.set_drone_LED(0,0,0,100)
 drone.send_absolute_position(0, 0, 1.5, 3, 0, 0)
-#drone.hover(5)
 drone.set_yaw(0)
-drone.move_forward(distance=1.54, units="m", speed=1)
+drone.move_forward(distance=2, units="m", speed=1)
 drone.set_yaw(0)
 
 #Go Down, make sure we are aligned with top panel hole
-drone.send_absolute_position(1.54, 0, 0.6, 0.5, 0, 0)
+drone.send_absolute_position(2, 0, 0.6, 0.5, 0, 0)
 drone.set_yaw(0)
-drone.send_absolute_position(1.54, 0, 0.6, 0.5, 0, 0)
+drone.send_absolute_position(2, 0, 0.6, 0.5, 0, 0)
 
 #Get First Mat Color, and show by changing both drone and controller colors, and printing RGB values
 color_data = drone.get_back_color("rgb")
@@ -28,14 +27,14 @@ drone.set_drone_LED(*color_data, brightness = 255)
 drone.set_controller_LED(*color_data, brightness = 255)
 
 #Ensure alignment, and Go Through Smaller Top Hole in Panel
-drone.send_absolute_position(1.54, 0, 0.6, 0.5, 0, 0)
-drone.send_absolute_position(3.14, 0, 0.6, 0.5, 0, 0)
+drone.send_absolute_position(2, 0, 0.63, 0.5, 0, 0)
+drone.send_absolute_position(3.25, 0, 0.63, 0.5, 0, 0)
 drone.set_yaw(0)
-drone.send_absolute_position(3.14, 0.4, 0.6, 0.5, 0, 0)
+drone.send_absolute_position(3.25, 0.4, 0.63, 0.5, 0, 0)
 drone.set_yaw(0)
-drone.send_absolute_position(4.41, 0.4, 0.6, 0.5, 0, 0)
+drone.send_absolute_position(4.55, 0.4, 0.63, 0.5, 0, 0)
 drone.set_yaw(0)
-drone.send_absolute_position(4.41, 0, 0.6, 0.5, 0, 0)
+drone.send_absolute_position(4.55, 0, 0.63, 0.5, 0, 0)
 drone.set_yaw(0)
 
 #Get Second Mat Color, and show by changing both drone and controller colors, and printing RGB values
@@ -44,6 +43,21 @@ print(color_data2)
 drone.set_drone_LED(*color_data2, brightness = 255)
 drone.set_controller_LED(*color_data2, brightness = 255)
 
+#Go through wind tunnel
+drone.send_absolute_position(4.55, 0, 160, 0.5, 0, 0)
+drone.set_yaw(0)
+drone.send_absolute_position(6.35, 0, 160, 0.5, 0, 0)
+drone.set_yaw(0)
+drone.send_absolute_position(4.55, 0, 160, 0.5, 0, 0)
+drone.set_yaw(0)
+drone.send_absolute_position(6.35, 0, 160, 0.5, 0, 0)
+drone.set_yaw(0)
+drone.send_absolute_position(4.55, 0, 160, 0.5, 0, 0)
+drone.set_yaw(0)
+drone.send_absolute_position(0.45, 0, 160, 0.5, 0, 0)
+drone.set_yaw(0)
+drone.send_absolute_position(0.45, 0, 0.9, 0.5, 0, 0)
+drone.set_yaw(0)
 #End code with a land and close
 drone.land()
 
